674772b828fc8d47153022d970505d61fa59e1e8
David Rosado Valerio <rosadovalerio.d@gmail.com>
2025-08-26 12:45:49 -0500

    feat: handle streaming response

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -3,6 +3,7 @@ import { HomePage } from "../pages/home/Home";
 import { LoginPage } from "../pages/login/Login";
 import { App } from "../App";
 import { GuardRoute } from "./GuardRoute";
+import HomeStreaming from "../pages/home/HomeStreaming";
 
 export const router = createHashRouter([
   {
@@ -21,7 +22,8 @@ export const router = createHashRouter([
         path: "home",
         element: (
           <GuardRoute>
-            <HomePage />
+            {/* <HomePage /> */}
+            <HomeStreaming />
           </GuardRoute>
         ),
       },
