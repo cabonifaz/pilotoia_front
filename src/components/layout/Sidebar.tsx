c4290333a7ffdb1c71d21b3e800cec13c8ae4d89
Gustavo Moreno <lpc9720@hotmail.com>
2025-11-13 17:25:51 -0500

    routes

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
-import { MessageCircle, Clipboard, Menu } from 'lucide-react';
+import { MessageCircle, Clipboard, Menu, Building2 } from 'lucide-react';
 import { Button } from '../shadcn/button';
 import { cn } from '@/lib/utils';
 import { ChatListSidebar } from './ChatListSidebar';
@@ -39,6 +39,7 @@ const Sidebar = ({
   const navigationItems = [
     { path: '/rag', label: 'Chat', icon: MessageCircle, allowedRoles: [1, 2, 3] },
     { path: '/upload', label: 'Documentos', icon: Clipboard, allowedRoles: [1, 2] },
+    { path: '/company', label: 'Compañias', icon: Building2, allowedRoles: [1] },
   ].filter(item => {
     if (!item.allowedRoles) return true; // si no tiene restricción, se muestra
     return item.allowedRoles.includes(user?.id_tipo_rol ?? -1); // compara rol
