0e4b916f4bff310ada7f04699235964da4687128
AshKuraine <lpc9720@hotmail.com>
2025-12-05 16:35:42 -0500

    updated types

--- src/types/aiConfig.ts
+++ src/types/aiConfig.ts
@@ -12,9 +12,7 @@ export interface ChatContext {
   user_id: number;
   user: string;
   company_id: number;
-  company: string;
   area_id: number;
-  area: string;
   id_ia_area: number;
   chat_id?: number | null;
   titulo?: string;
