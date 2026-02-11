89ec0d80af0a2b677bbd8fc81e95a59eb2117ec5
AshKuraine <lpc9720@hotmail.com>
2026-02-11 17:58:08 -0500

    removed phone codes module

--- src/api/phoneCodesApi.ts
+++
@@ -1,9 +0,0 @@
-import apiClient from './apiClient';
-import type { GetPhoneCodesResponse } from '@/types/phoneCodes';
-
-export const getPhoneCodes = async (): Promise<GetPhoneCodesResponse> => {
-  const response = await apiClient.get<GetPhoneCodesResponse>(
-    '/v1/phone_code/get_phone_codes'
-  );
-  return response.data;
-};
